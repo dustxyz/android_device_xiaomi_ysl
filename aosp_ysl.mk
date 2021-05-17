99c9a57549b38fa01572ffee4a87ac2af0091ca1
dustxyz <dustxyz@criptext.com>
2021-05-17 11:31:26 +0000

    ysl: HyconOS BringUp
    Signed-off-by: dustxyz <dustxyz@criptext.com>

--- aosp_ysl.mk
+++ aosp_ysl.mk
@@ -24,19 +24,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
-# Inherit from mido device
+# Inherit from ysl device
 $(call inherit-product, device/xiaomi/ysl/device.mk)
 
-# Inherit some common RevengeOS stuff.
-$(call inherit-product, vendor/revengeos/config/common.mk)
+# Inherit some common HyconOS stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-# Inherit some common RevengeOS flags
-REVENGEOS_BUILDTYPE := OFFICIAL
+# Inherit some common HyconOS flags
+CUSTOM_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
-PRODUCT_NAME := revengeos_ysl
+PRODUCT_NAME := aosp_ysl
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi S2
